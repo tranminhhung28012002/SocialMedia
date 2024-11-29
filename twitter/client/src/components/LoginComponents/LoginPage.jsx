c9c5ed99f5b162566dca730152fa75fdeabcac52
tranminhhung28012002 <himden9999@gmail.com>
2024-11-29 11:43:43 +0700

    them login admin

--- twitter/client/src/components/LoginComponents/LoginPage.jsx
+++ twitter/client/src/components/LoginComponents/LoginPage.jsx
@@ -13,7 +13,9 @@ const LoginPage = () => {
   const { setUser, user } = useAuth()
   console.log(user)
   const navigate = useNavigate()
+
   const handleSubmit = (values) => {
+    // Thử đăng nhập với API user thường trước
     axiosInstance
       .post('/api/login', values)
       .then((res) => {
@@ -22,38 +24,46 @@ const LoginPage = () => {
         axiosInstance
           .get('/api/me')
           .then((res) => {
-            setUser(res.data.result)
-            navigate('/home')
+            const userData = res.data.result
+            setUser(userData)
+            if (!userData.role) {
+              navigate('/home')
+            } else {
+              navigate('/admin')
+            }
           })
           .catch((error) => {
             console.log(error)
           })
       })
-      .catch((error) => {
-        console.log(error)
-        for (const key of Object.keys(error?.response?.data?.errors)) {
-          messageApi.error(error?.response?.data?.errors[key].msg, 2)
-        }
+      .catch((userError) => {
+        console.log(userError)
+        messageApi.error('Login failed for user. Trying admin login...')
+        axiosInstance
+          .post('/admin/login', values)
+          .then((res) => {
+            localStorage.setItem('access_token', res.data.access_token)
+            //đoạn này có thể thêm api lấy thông tin admin để view ra
+            navigate('/admin')
+          })
+          .catch((adminError) => {
+            console.log(adminError)
+
+            // Hiển thị lỗi nếu cả hai API đều thất bại
+            for (const key of Object.keys(adminError?.response?.data?.errors || {})) {
+              messageApi.error(adminError?.response?.data?.errors[key].msg, 2)
+            }
+          })
       })
   }
 
   return (
-    <main className=' flex justify-around items-center px-20 text-lg  max-md:px-5 h-[100vh] '>
-      <img
-        src='/images/Premium Vector _ Social media marketing concept.jpg '
-        alt='Social Media'
-        className=' object-contain rounded-lg lg:w-5/12 display-none-md'
-      />
+    <main className='flex overflow-hidden items-center justify-around text-lg bg-white'>
+      <img loading='lazy' src='./images/VAlogin.svg' className='object-cover' alt='Twitter logo' />
       <div className='flex flex-col max-w-full w-[450px]'>
         {contextHolder}
-        <img
-          loading='lazy'
-          src='https://cdn.builder.io/api/v1/image/assets/TEMP/bef80e460a0946a1cac96cf3ab2dc17be65b97af1ad29bcda78b3491d138c5d6?placeholderIfAbsent=true&apiKey=ee3b4c55f4e940649da4e87de99f1704'
-          className='object-contain aspect-[1.22] w-[50px] mx-auto'
-          alt='Twitter logo'
-        />
         <Title level={1} className='self-start mt-9 text-5xl font-black text-black max-md:ml-2.5'>
-          Welcome to Twitter
+          Log in to VAsocial
         </Title>
         <Form form={form} onFinish={handleSubmit}>
           <Form.Item
@@ -75,7 +85,7 @@ const LoginPage = () => {
           </Form.Item>
           <Form.Item>
             <Button className='mt-6 w-full h-12' htmlType='submit'>
-              Sign Up
+              Log In
             </Button>
           </Form.Item>
         </Form>
