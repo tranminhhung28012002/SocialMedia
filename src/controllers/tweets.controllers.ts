1909f9d12acda8bb3dd97953d67244b107e5dc74
phamvantu090303 <phamtu090303@gmail.com>
2024-12-04 14:17:25 +0700

    Update tweets.controllers.ts with latest changes

--- src/controllers/tweets.controllers.ts
+++ src/controllers/tweets.controllers.ts
@@ -1,14 +1,9 @@
 import { TokenPayload } from '../models/requests/Users.Requests'
 import tweetsService from '../services/tweets.services'
 import { TweetType } from '../constants/enums'
-import { Request, Response } from 'express'
+import { Response } from 'express'
 import CustomRequest from '../type'
 import { TweetPanamQuenyPagination, TweetRequestBody } from '../models/requests/Tweer.requests'
-import OpenAI from 'openai'
-
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY // Thay bằng API key của bạn
-})
 
 export const createTweetController = async (req: CustomRequest<TweetRequestBody>, res: Response) => {
   const { user_id } = req.decoded_authorization as TokenPayload
@@ -98,4 +93,63 @@ export const getTweetChildrenController = async (
     }
   })
 }
+export const getReTweetController = async (
+  req: CustomRequest<TweetPanamQuenyPagination>, // code chạy tạm
 
+  res: Response
+) => {
+  const tweet_type = Number(req.query.tweet_type as string) as TweetType // Chuyển đổi kiểu query
+
+  const limit = Number(req.query.limit as string)
+
+  const page = Number(req.query.page as string)
+
+  const user_id = req.decoded_authorization?.user_id
+
+  const { total, tweets } = await tweetsService.getReTweet({
+    user_id,
+
+    tweet_id: req.params.tweet_id,
+
+    tweet_type,
+
+    limit,
+
+    page
+  })
+
+  return res.json({
+    message: 'GET ReTweet thành công',
+
+    result: {
+      tweets,
+
+      tweet_type,
+
+      limit,
+
+      page,
+
+      total_page: Math.ceil(total / limit) // Tính số trang
+    }
+  })
+}
+export const getNewFeedsController = async (req: CustomRequest<TweetPanamQuenyPagination>, res: Response) => {
+  const user_id = req.decoded_authorization?.user_id as string
+  const limit = Number(req.query.limit)
+  const page = Number(req.query.page)
+  const result = await tweetsService.getNewFeeds({
+    user_id,
+    limit,
+    page
+  })
+  return res.json({
+    message: 'Get New Feeds Thành Công',
+    result: {
+      tweets: result.tweets,
+      limit,
+      page,
+      total_page: Math.ceil(result.total / limit) // Tính số trang
+    }
+  })
+}
