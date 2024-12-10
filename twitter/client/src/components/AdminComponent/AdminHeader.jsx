290a248c354d56ec392fb4cfdee1d30f3f597ad9
tranminhhung28012002 <himden9999@gmail.com>
2024-12-10 16:45:32 +0700

    thay doi login

--- twitter/client/src/components/AdminComponent/AdminHeader.jsx
+++
@@ -1,9 +0,0 @@
-function AdminHeader() {
-  return (
-    <header className='bg-sky-500 text-black p-2'>
-      <nav className='flex items-center '></nav>
-    </header>
-  )
-}
-
-export default AdminHeader
