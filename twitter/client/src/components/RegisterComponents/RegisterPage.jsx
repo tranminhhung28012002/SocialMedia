9d57ed6e188aee35503db4e10602963539531012
tranminhhung28012002 <himden9999@gmail.com>
2024-12-10 17:09:53 +0700

    Register 1.0

--- twitter/client/src/components/RegisterComponents/RegisterPage.jsx
+++ twitter/client/src/components/RegisterComponents/RegisterPage.jsx
@@ -3,7 +3,6 @@ import { toast } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import Button from '../Button'
 import Link from '../Link'
-import { axiosInstance } from '../../axios'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 
